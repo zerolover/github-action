f570f9f0e72c7117bb50f3f4f6ee3546850c8c80
zerolover <zerolover@msn.cn>
2025-01-01 22:27:22 +0800

    update ci

--- script/build-deps-ubuntu.sh
+++ script/build-deps-ubuntu.sh
@@ -4,11 +4,15 @@ set -e
 ROOT=/workspace
 LO=$ROOT/core
 DP=$ROOT/deps
+LO_3RD=$LO/workdir/UnpackedTarball
 
+# build dependencies
 cd $LO
 ./autogen.sh --without-java --without-doxygen --without-help --disable-online-update --disable-ccache
 make fetch
 make UnpackedTarball
+
+make argon2
 make boost
 make box2d
 make curl
@@ -16,26 +20,58 @@ make dtoa
 make epoxy
 make expat
 make harfbuzz
+make icu
+make lcms2
+make libexttextcat
+make liblangtag
+make libpng
 make libtiff
-make zxing
+make libwebp
 make zxcvbn-c
+make zxing
 
-ls -alh $LO/workdir/UnpackedTarball/
+# debug
+ls -alhd $LO/workdir/UnpackedTarball/*/
 ls -alh $LO/instdir/program/
 
+# copy libraries
 cd $ROOT
 mkdir -p $DP/libs
+
+cp -a $LO_3RD/argon2/libargon2.a $DP/libs/
+cp -a $LO_3RD/curl/lib/.libs/lib*.so* $DP/libs/
+cp -a $LO_3RD/harfbuzz/src/.libs/libharfbuzz.a $DP/libs/
+cp -a $LO_3RD/icu/source/lib/lib*.so* $DP/libs/
+cp -a $LO_3RD/lcms2/src/.libs/lib*.so* $DP/libs/
+cp -a $LO_3RD/libexttextcat/src/.libs/libexttextcat-2.0.a $DP/libs/
+cp -a $LO_3RD/liblangtag/liblangtag/.libs/liblangtag*.so* $DP/libs/
+cp -a $LO_3RD/libtiff/libtiff/.libs/libtiff.a $DP/libs/
+cp -a $LO_3RD/libwebp/sharpyuv/.libs/libsharpyuv.a $DP/libs/
+cp -a $LO_3RD/libwebp/src/.libs/libwebp.a $DP/libs/
+
+cp $LO/instdir/program/libepoxy.so $DP/libs/
 cp $LO/workdir/LinkTarget/StaticLibrary/lib*.a $DP/libs/
-cp -a $LO/workdir/UnpackedTarball/liblangtag/liblangtag/.libs/liblangtag*.so* $DP/libs/
-cp -a $LO/workdir/UnpackedTarball/icu/source/lib/lib*.so* $DP/libs/
-cp $LO/workdir/UnpackedTarball/curl/lib/.libs/libcurl.so $DP/libs/
-cp $LO/workdir/UnpackedTarball/lcms2/src/.libs/liblcms2.so $DP/libs/
-cp $LO/workdir/UnpackedTarball/libwebp/src/.libs/libwebp.a $DP/libs/
-cp $LO/workdir/UnpackedTarball/libwebp/sharpyuv/.libs/libsharpyuv.a $DP/libs/
-cp $LO/workdir/UnpackedTarball/harfbuzz/src/.libs/libharfbuzz.a $DP/libs/
 cp /usr/lib/x86_64-linux-gnu/libstdc++.so.6.0.32 $DP/libs/libstdc++.so.6
 
+# copy headers
 mkdir -p $DP/inc
-cp $LO/workdir/UnpackedTarball/libtiff/libtiff/*.h $DP/inc/
-cp -r $LO/workdir/UnpackedTarball/boost/boost $DP/inc/
-cp -r $LO/workdir/UnpackedTarball/curl/include/curl $DP/inc/
+mkdir -p $DP/inc/liblangtag
+
+cp -r $LO_3RD/argon2/include/*.h $DP/inc/
+cp -r $LO_3RD/boost/boost $DP/inc/
+cp -r $LO_3RD/box2d/include/box2d $DP/inc/
+cp -r $LO_3RD/curl/include/curl $DP/inc/
+cp -r $LO_3RD/dragonbox/include/dragonbox $DP/inc/
+cp -r $LO_3RD/dtoa/include/*.h $DP/inc/
+cp -r $LO_3RD/frozen/include/frozen $DP/inc/
+cp -r $LO_3RD/harfbuzz/src/*.h $DP/inc/
+cp -r $LO_3RD/icu/source/common/unicode $DP/inc/
+cp -r $LO_3RD/icu/source/i18n/unicode $DP/inc/
+cp -r $LO_3RD/icu/source/io/unicode $DP/inc/
+cp -r $LO_3RD/lcms2/include/*.h $DP/inc/
+cp -r $LO_3RD/libexttextcat/src/*.h $DP/inc/
+cp -r $LO_3RD/liblangtag/liblangtag/*.h $DP/inc/liblangtag/
+cp -r $LO_3RD/libtiff/libtiff/*.h $DP/inc/
+cp -r $LO_3RD/libwebp/src/webp $DP/inc/
+cp -r $LO_3RD/mdds/include/mdds $DP/inc/
+cp -r $LO_3RD/zxcvbn-c/zxcvbn.h $DP/inc/
---
+++ script/build-deps-windows.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+set -e
+
+ROOT=/
+LO=/core
+DP=/deps
+LO_3RD=$LO/workdir/UnpackedTarball
+pwd
+
+ls -alh $ROOT
+
+# build dependencies
+cd $LO
+pwd
+/opt/lo/bin/make fetch
+/opt/lo/bin/make UnpackedTarball
+
+/opt/lo/bin/make argon2
+/opt/lo/bin/make boost
+/opt/lo/bin/make box2d
+# /opt/lo/bin/make curl
+# /opt/lo/bin/make dtoa
+# /opt/lo/bin/make epoxy
+# /opt/lo/bin/make expat
+# /opt/lo/bin/make harfbuzz
+# /opt/lo/bin/make icu
+# /opt/lo/bin/make lcms2
+# /opt/lo/bin/make libexttextcat
+# /opt/lo/bin/make liblangtag
+# /opt/lo/bin/make libpng
+# /opt/lo/bin/make libtiff
+# /opt/lo/bin/make libwebp
+# /opt/lo/bin/make zxcvbn-c
+# /opt/lo/bin/make zxing
+
+# # debug
+# ls -alhd $LO/workdir/UnpackedTarball/*/
+# ls -alh $LO/instdir/program/
+
+# # copy libraries
+# cd $ROOT
+# mkdir -p $DP/libs
+
+# cp -a $LO_3RD/argon2/libargon2.a $DP/libs/
+# cp -a $LO_3RD/curl/lib/.libs/lib*.so* $DP/libs/
+# cp -a $LO_3RD/harfbuzz/src/.libs/libharfbuzz.a $DP/libs/
+# cp -a $LO_3RD/icu/source/lib/lib*.so* $DP/libs/
+# cp -a $LO_3RD/lcms2/src/.libs/lib*.so* $DP/libs/
+# cp -a $LO_3RD/libexttextcat/src/.libs/libexttextcat-2.0.a $DP/libs/
+# cp -a $LO_3RD/liblangtag/liblangtag/.libs/liblangtag*.so* $DP/libs/
+# cp -a $LO_3RD/libtiff/libtiff/.libs/libtiff.a $DP/libs/
+# cp -a $LO_3RD/libwebp/sharpyuv/.libs/libsharpyuv.a $DP/libs/
+# cp -a $LO_3RD/libwebp/src/.libs/libwebp.a $DP/libs/
+
+# cp $LO/instdir/program/libepoxy.so $DP/libs/
+# cp $LO/workdir/LinkTarget/StaticLibrary/lib*.a $DP/libs/
+# cp /usr/lib/x86_64-linux-gnu/libstdc++.so.6.0.32 $DP/libs/libstdc++.so.6
+
+# # copy headers
+# mkdir -p $DP/inc
+# mkdir -p $DP/inc/liblangtag
+
+# cp -r $LO_3RD/argon2/include/*.h $DP/inc/
+# cp -r $LO_3RD/boost/boost $DP/inc/
+# cp -r $LO_3RD/box2d/include/box2d $DP/inc/
+# cp -r $LO_3RD/curl/include/curl $DP/inc/
+# cp -r $LO_3RD/dragonbox/include/dragonbox $DP/inc/
+# cp -r $LO_3RD/dtoa/include/*.h $DP/inc/
+# cp -r $LO_3RD/frozen/include/frozen $DP/inc/
+# cp -r $LO_3RD/harfbuzz/src/*.h $DP/inc/
+# cp -r $LO_3RD/icu/source/common/unicode $DP/inc/
+# cp -r $LO_3RD/icu/source/i18n/unicode $DP/inc/
+# cp -r $LO_3RD/icu/source/io/unicode $DP/inc/
+# cp -r $LO_3RD/lcms2/include/*.h $DP/inc/
+# cp -r $LO_3RD/libexttextcat/src/*.h $DP/inc/
+# cp -r $LO_3RD/liblangtag/liblangtag/*.h $DP/inc/liblangtag/
+# cp -r $LO_3RD/libtiff/libtiff/*.h $DP/inc/
+# cp -r $LO_3RD/libwebp/src/webp $DP/inc/
+# cp -r $LO_3RD/mdds/include/mdds $DP/inc/
+# cp -r $LO_3RD/zxcvbn-c/zxcvbn.h $DP/inc/
